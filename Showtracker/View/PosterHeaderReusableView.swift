5bc4635a6e2caae153d1eaf008bb41da20e07a56
V3RT1AG0 <adityarajpednekar4@gmail.com>
2019-08-07 14:49:38 -0400

    Completed anime screen

---
+++ Showtracker/View/PosterHeaderReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  PosterHeaderReusableView.swift
+//  Showtracker
+//
+//  Created by Adityaraj Pednekar on 8/7/19.
+//  Copyright © 2019 Adityaraj Pednekar. All rights reserved.
+//
+
+import UIKit
+
+class PosterHeaderReusableView: UICollectionReusableView {
+        
+}
